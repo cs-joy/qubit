879dde01ab0853849e02a5514e3e3c6f9c98cb27
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-15 03:34:50 +0600

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,7 +27,7 @@ const Layout = ({ location, title, children }) => {
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://www.sharpsolutions.com">cs. joy</a>
       </footer>
     </div>
   )
